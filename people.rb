f4bb12be2198392f88c9ecf84f914b2e9e597508
Josh Comeau <joshwcomeau@gmail.com>
2014-03-26 16:32:12 -0400

    Fixed the comments

--- people.rb
+++ people.rb
@@ -1,14 +1,3 @@
-# Let's start by creating two classes: one called Student and another called Instructor.
-# The student class has a method called learn that outputs "I get it!".
-# The instructor class has a method called teach that outputs "Everything in Ruby is an Object".
-# Both the instructor and the student have names. We know that instructors and students are both people. Create a parent Person class that contains 
-# the attribute name and an initializer to set the name.
-# Both the instructor and the student should also be able to do a greeting, like "Hi, my name is #{name}". Where's the best place to put this common 
-# method?
-# Create an instance of Instructor whose name is "Chris" and call his greeting.
-# Create an instance of Student whose name is "Cristina" and call her greeting.
-# Call the teach method on your instructor instance and call the learn method on your student. Next, call the teach method on your student instance. 
-# What happens? Why doesn't that work? Leave a comment in your program explaining why.
 class Person
   def initialize(name)
     @name = name
@@ -43,6 +32,7 @@ Christina.greeting
 Christina.learn
 
 Christina.teach
-# => raises an 'undefined method' error because .teach is a method for the Instructor class, and we're trying to call it on a Student instance.
+# => raises an 'undefined method' error because .teach is a method belonging to the 
+# Instructor class, and we're trying to call it on a Student instance.
 
 
